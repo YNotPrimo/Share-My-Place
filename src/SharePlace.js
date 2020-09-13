a96f81470745a87e215b674bea34e78c47754d16
Ivalin Kolev <ivalinkolev0@gmail.com>
2020-09-13 16:58:15 +0300

    Show Map functionality done

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,12 +1,20 @@
 import { Modal } from "./UI/Modal";
-
+import { Map } from "./UI/Map";
 class PlaceFinder {
 	constructor() {
 		const addressForm = document.querySelector("form");
 		const locateUserBtn = document.getElementById("locate-btn");
 
-		locateUserBtn.addEventListener("click", this.locateUserHandler);
+		locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
 		addressForm.addEventListener("submit", this.findAddressHandler);
+	}
+
+	selectPlace(coordinates) {
+		if (this.map) {
+			this.map.render(coordinates);
+		} else {
+			this.map = new Map(coordinates);
+		}
 	}
 
 	locateUserHandler() {
@@ -15,23 +23,25 @@ class PlaceFinder {
 				"Location feature is not available in your browser - please use a more modern browser or manually enter an address."
 			);
 			return;
-    }
-    
-    const modal = new Modal("loading-modal-content", "Loading location - please wait!");
+		}
 
-    modal.show();
+		const modal = new Modal(
+			"loading-modal-content",
+			"Loading location - please wait!"
+		);
+
+		modal.show();
 
 		navigator.geolocation.getCurrentPosition(
 			successResult => {
-        modal.hide();
+				modal.hide();
 				const coordinates = {
 					lat: successResult.coords.latitude,
 					lng: successResult.coords.longitude,
 				};
-				console.log(coordinates);
+				this.selectPlace(coordinates);
 			},
 			error => {
-        
 				alert(
 					"Could not locate you unfortunately. Please enter an address manually!"
 				);
---
+++ src/UI/Map.js
@@ -0,0 +1,28 @@
+export class Map {
+	constructor(coords) {
+		// this.coordinates = coords;
+		this.render(coords);
+	}
+
+	render(coordinates) {
+		// if (!google) {
+		// 	alert("Could not load maps library");
+		// 	return;
+		// }
+
+        document.getElementById('map').innerHTML = ''; // clear the <p> in the <div id="map">
+     
+        const map = new ol.Map({
+          target: 'map',
+          layers: [
+            new ol.layer.Tile({
+              source: new ol.source.OSM()
+            })
+          ],
+          view: new ol.View({
+            center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+            zoom: 16
+          })
+        });
+	}
+}
